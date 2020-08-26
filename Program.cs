ddc6ff0844eb479fbe333744f3ef19ef7c43b0d1
irwinet <irwinestradatorres@gmail.com>
2020-08-26 09:48:20 -0500

    Control Statements

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSharp03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello WorldDo you want to go out?");
+            var input = Console.ReadLine();
+
+            switch(input){
+                case "Y":
+                {
+                    Console.WriteLine("Ok, exit");
+                    break;
+                }
+                case "X":
+                case "N":
+                {
+                    Console.WriteLine("Ok exit xD");
+                    break;
+                }
+                default:
+                {
+                    Console.WriteLine("Invalid Option");
+                    break;
+                }
+            }
+        }
+    }
+}
